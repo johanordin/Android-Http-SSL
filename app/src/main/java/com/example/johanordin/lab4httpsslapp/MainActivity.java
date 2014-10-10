51e108eed3b921bd07531e0de1540dbe9254c37c
johno855 <johno855@student.liu.se>
2014-10-10 08:49:49 +0200

    anvant den mall som finns tillganglig och skapat ytterligare en knapp

--- app/src/main/java/com/example/johanordin/lab4httpsslapp/MainActivity.java
+++ app/src/main/java/com/example/johanordin/lab4httpsslapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.johanordin.lab4httpsslapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +15,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    public ProgressDialog loadingdialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,20 +26,89 @@ public class MainActivity extends Activity {
         Log.d("Http/SSL log", "This is a test");
 
         Button knapp1 = (Button)findViewById(R.id.knapp1);
-
         knapp1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+
+                buttonAction("https://www.liu.se/");
+                /*
                 Toast.makeText(getApplicationContext(),
                         "Knapp1 är tryckt", Toast.LENGTH_LONG).show();
+                */
+            }
+        });
+        Button knapp2 = (Button)findViewById(R.id.knapp2);
+        knapp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                buttonAction("https://tal-front.itn.liu.se/");
 
             }
         });
 
+    }
 
 
 
+    public void buttonAction(final String s_url) {
+
+        loadingdialog = ProgressDialog.show(MainActivity.this, "", "Loading...", true);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //kod ska in här
+
+
+
+                    showAlert(s_url, "HTTP Status: ");
+                } catch (Exception e) {
+                    //showAlert()
+                    e.printStackTrace();
+                    showAlert(s_url, "ERROR: " + e.getMessage());
+                }
+
+                loadingdialog.dismiss();
+            }
+        });
+
+        thread.start();
+    }
+
+    public void showToast(final String toast) {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public void showAlert(final String header, final String message) {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle(header);
+                alertDialog.setMessage(message);
+                alertDialog.setCancelable(false);
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
     }
 
 
